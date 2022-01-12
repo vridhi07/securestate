16c76e92b899fba5e24e609a2dd1e7db300e4d67
shubhametech <shubham@securestate.io>
2022-01-12 14:22:37 +0530

    changes to pentest scope

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
