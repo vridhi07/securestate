8cd533c0093841fa6becfdb111e9bb0a929d637f
shubhametech <shubham@securestate.io>
2022-02-14 18:29:57 +0530

    added desing for form

--- src/Component/Asset/AssetFileModal.js
+++ src/Component/Asset/AssetFileModal.js
@@ -2,7 +2,7 @@ import * as React from "react";
 // import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import pdfImage from "../../constantData/images/pdf_image.png";
+// import pdfImage from "../../constantData/images/pdf_image.png";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 import CloseIcon from "@mui/icons-material/Close";
 export default function FormDialog({
@@ -32,120 +32,119 @@ export default function FormDialog({
   return (
     <div>
       <Dialog open={isFileModalOpen} maxWidth={"lg"}>
-        <form
-          className="flex flex-col  px-5 pt-6  pb-10 md:px-16"
-          onSubmit={handleSubmit}
-        >
-          <button
-            type="button"
-            onClick={closeFileModal}
-            className="absolute top-4 right-4"
-          >
-            <CloseIcon />
-          </button>
-          <h2 className="my-4 text-center text-lg font-bold text-gray-700 ">
-            Files
-          </h2>
-          <TextField
-            id="file_Name"
-            label="Name"
-            value={fileName}
-            onChange={handleChange}
-            name="fileName"
-            sx={{ mb: 3 }}
-            fullWidth
-            required
-          />
-          <TextField
-            id="description"
-            label="Description"
-            name="description"
-            value={description}
-            multiline
-            rows={3}
-            onChange={handleChange}
-            sx={{ mb: 3 }}
-            fullWidth
-            required
-          />
-          <input
-            id="contained-button-file"
-            // multiple
-            type="file"
-            name="file"
-            className="hidden"
-            onChange={handleFile}
-          />
-
-          <div className="mt-3">
-            <div
-              className="m-0 flex h-[150px] w-[400px] flex-col items-center justify-center border-2 border-dashed border-gray-500 "
-              onDragOver={dragOver}
-              onDragEnter={dragEnter}
-              onDragLeave={dragLeave}
-              onDrop={fileDrop}
-            >
-              <label htmlFor="file" className="hover:cursor-pointer ">
-                <UploadFileIcon sx={{ fontSize: "4rem", color: "#606060" }} />
-              </label>
-
-              <input
-                type="file"
-                id="file"
-                className="hidden"
-                onChange={handleFile}
-              />
-
-              <p>Drag & Drop files here or click to upload</p>
-            </div>
-            <div>
-              {file &&
-                file.map((item, i) => {
-                  return (
-                    <div key={i} className="my-3 flex justify-between">
-                      <div>
-                        {item[0].type === "application/pdf" ? (
-                          <iframe
-                            src={URL.createObjectURL(item[0])}
-                            // alt="pdf image"
-                            className="h-6 w-14"
-                          />
-                        ) : (
-                          <img
-                            src={URL.createObjectURL(item[0])}
-                            alt="pdf image"
-                            className="h-5 w-5"
-                          />
-                        )}
-                      </div>
-                      <div className="flex">
-                        <p className="mr-3">{item[0].name}</p>
-                        <button
-                          onClick={() => cancelFile(i)}
-                          className="text-red-400"
-                        >
-                          x
-                        </button>
-                      </div>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-          <div className="mt-5 flex items-center justify-between">
-            <button
-              type="submit"
-              className="rounded-md bg-[#606060] px-5 py-2 text-white"
-            >
-              Submit
-            </button>
+        <form className="flex flex-col  " onSubmit={handleSubmit}>
+          <header className="flex justify-between bg-orange-cus-1 py-3  px-4 text-white">
+            <h2 className=" text-center text-lg font-bold  ">Files</h2>
             <button
               type="button"
-              className="rounded-md bg-[#606060] px-5 py-2 text-white"
               onClick={closeFileModal}
+              // className="absolute top-4 right-4"
             >
-              Cancel
+              <CloseIcon />
             </button>
+          </header>
+          <div className="px-8 py-4">
+            <TextField
+              id="file_Name"
+              label="Name"
+              value={fileName}
+              onChange={handleChange}
+              name="fileName"
+              sx={{ mb: 3 }}
+              fullWidth
+              required
+            />
+            <TextField
+              id="description"
+              label="Description"
+              name="description"
+              value={description}
+              multiline
+              rows={3}
+              onChange={handleChange}
+              sx={{ mb: 3 }}
+              fullWidth
+              required
+            />
+            <input
+              id="contained-button-file"
+              // multiple
+              type="file"
+              name="file"
+              className="hidden"
+              onChange={handleFile}
+            />
+
+            <div className="mt-3">
+              <div
+                className="m-0 flex h-[150px] w-full flex-col items-center justify-center border-2 border-dashed border-gray-500 "
+                onDragOver={dragOver}
+                onDragEnter={dragEnter}
+                onDragLeave={dragLeave}
+                onDrop={fileDrop}
+              >
+                <label htmlFor="file" className="hover:cursor-pointer ">
+                  <UploadFileIcon sx={{ fontSize: "4rem", color: "#606060" }} />
+                </label>
+
+                <input
+                  type="file"
+                  id="file"
+                  className="hidden"
+                  onChange={handleFile}
+                />
+
+                <p>Drag & Drop files here or click to upload</p>
+              </div>
+              <div>
+                {file &&
+                  file.map((item, i) => {
+                    return (
+                      <div key={i} className="my-3 flex justify-between">
+                        <div>
+                          {item[0].type === "application/pdf" ? (
+                            <iframe
+                              src={URL.createObjectURL(item[0])}
+                              // alt="pdf image"
+                              className="h-6 w-14"
+                            />
+                          ) : (
+                            <img
+                              src={URL.createObjectURL(item[0])}
+                              alt="pdf image"
+                              className="h-5 w-5"
+                            />
+                          )}
+                        </div>
+                        <div className="flex">
+                          <p className="mr-3">{item[0].name}</p>
+                          <button
+                            onClick={() => cancelFile(i)}
+                            className="text-red-400"
+                          >
+                            x
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+            <div className="mt-5 flex items-center justify-between">
+              <button
+                type="submit"
+                className="rounded-md bg-[#606060] px-5 py-2 text-white"
+              >
+                Submit
+              </button>
+              <button
+                type="button"
+                className="rounded-md bg-[#606060] px-5 py-2 text-white"
+                onClick={closeFileModal}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </form>
       </Dialog>
