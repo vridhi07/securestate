d4458841c2469c19b1b0f016fb0a30bad7a36e4c
shubhametech <shubham@securestate.io>
2022-02-14 18:02:14 +0530

    added desgin for the form

--- src/Component/Asset/HistoryForm.js
+++ src/Component/Asset/HistoryForm.js
@@ -17,68 +17,75 @@ export default function FormDialog({
   return (
     <div>
       <Dialog open={isHistoryOpen}>
-        <form className="relative px-5 py-2 md:px-20" onSubmit={handleSubmit}>
-          <button
-            type="button"
-            onClick={closeHistoryModal}
-            className="absolute top-4 right-4"
-          >
-            <CloseIcon />
-          </button>
-          <h2 className="mb-3 py-3 text-center text-lg font-bold text-gray-700">
-            Add Event
-          </h2>
-          <TextField
-            id="Event"
-            type={"text"}
-            label="Event"
-            variant="outlined"
-            name="event"
-            value={event}
-            onChange={handleEventChange}
-            fullWidth
-            required
-            sx={{ mb: 3 }}
-          />
-          <div className="mb-3 w-full">
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DesktopDatePicker
-                label="Date"
-                value={date}
-                minDate={new Date()}
-                onChange={handleDate}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-          </div>
-          <TextField
-            id="Description"
-            type={"text"}
-            label="Description"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            name="description"
-            value={description}
-            required
-            onChange={handleEventChange}
-            sx={{ mb: 3 }}
-          />
-          <div className="my-4 flex items-center justify-between">
-            <button
-              type="submit"
-              className="rounded-md bg-[#606060] px-7 py-2 tracking-wider"
-            >
-              Add
-            </button>
+        <form className="" onSubmit={handleSubmit}>
+          <header className="py-3text-white flex h-12 w-full items-center justify-between bg-orange-cus-1 px-5 text-white ">
+            <h2 className=" text-center text-lg font-bold  ">Add Event</h2>
             <button
               type="button"
-              className="rounded-md bg-[#606060] px-4 py-2 tracking-wider"
               onClick={closeHistoryModal}
+              // className="absolute top-4 right-4"
             >
-              Cancel
+              <CloseIcon />
             </button>
+          </header>
+          <div className="px-8 py-4">
+            <section className="my-2 grid grid-cols-4 gap-8">
+              <div className="col-span-4 md:col-span-2">
+                <TextField
+                  id="Event"
+                  type={"text"}
+                  label="Event"
+                  variant="outlined"
+                  name="event"
+                  value={event}
+                  onChange={handleEventChange}
+                  fullWidth
+                  required
+                  sx={{ mb: 3 }}
+                />
+              </div>
+              <div className="col-span-4 md:col-span-2">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DesktopDatePicker
+                    label="Date"
+                    value={date}
+                    minDate={new Date()}
+                    onChange={handleDate}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </div>
+            </section>
+
+            <TextField
+              id="Description"
+              type={"text"}
+              label="Description"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={4}
+              name="description"
+              value={description}
+              required
+              onChange={handleEventChange}
+              sx={{ mb: 3 }}
+            />
+            <div className="my-4 flex items-center justify-between">
+              <button
+                type="submit"
+                className="rounded-md bg-[#606060] px-7 py-2 tracking-wider text-gray-300"
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                className="rounded-md bg-[#606060] px-4 py-2 tracking-wider text-gray-300"
+                onClick={closeHistoryModal}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </form>
       </Dialog>
