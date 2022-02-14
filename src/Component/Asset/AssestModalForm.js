846a217a9428748959c08b5e87f89f58e21e7135
shubhametech <shubham@securestate.io>
2022-02-14 18:00:25 +0530

    addet design for this

--- src/Component/Asset/AssestModalForm.js
+++ src/Component/Asset/AssestModalForm.js
@@ -23,155 +23,152 @@ export default function AssetModal({
   } = assetForm;
 
   return (
-    <form className="px-10 pt-10 pb-12 relative" onSubmit={handleSubmitAsset}>
-      <button
-        type="button"
-        onClick={handleClose}
-        className="absolute top-4 right-4"
-      >
-        <CloseIcon />
-      </button>
-      <header className="mb-3">
+    <form onSubmit={handleSubmitAsset}>
+      <header className="flex justify-between bg-orange-cus-1 py-3  px-4 text-white">
         <h2 className="text-center text-2xl font-bold tracking-wider">
           {isEdit ? "Edit Asset" : "New Asset"}
         </h2>
+        <button type="button" onClick={handleClose}>
+          <CloseIcon />
+        </button>
       </header>
+      <div className="px-8 py-4">
+        <section className="my-2 grid grid-cols-4 gap-8">
+          <div className="col-span-4 md:col-span-2">
+            <TextField
+              name="asset_name"
+              id="AssetName"
+              className="h-11 w-full resize-none border-2 border-gray-400 py-2 px-2 focus:outline-none"
+              label="Asset Name"
+              variant="outlined"
+              size="small"
+              inputProps={{ maxLength: 80 }}
+              value={asset_name}
+              onChange={handleAssetForm}
+              required
+            />
+          </div>
+          <div className="col-span-4 md:col-span-2">
+            <FormControl fullWidth>
+              <InputLabel id="assetType">Asset Type</InputLabel>
+              <Select
+                aria-label="search select"
+                name="asset_type"
+                id="assetType"
+                size="small"
+                label="Asset Type"
+                value={asset_type}
+                onChange={handleAssetForm}
+                required
+              >
+                {AssetType.map((item) => {
+                  return (
+                    <MenuItem value={item} key={item}>
+                      {item}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </div>
+        </section>
 
-      <section className="grid grid-cols-4 gap-8 my-2">
-        <div className="md:col-span-2 col-span-4">
+        <section className="my-4 grid grid-cols-4 gap-8">
+          <div className="col-span-4 md:col-span-2">
+            <FormControl fullWidth>
+              <InputLabel id="Status">Status</InputLabel>
+              <Select
+                aria-label="search select"
+                labelId="Status"
+                name="status"
+                id="Status"
+                size="small"
+                label="Status"
+                value={status}
+                onChange={handleAssetForm}
+                required
+              >
+                <MenuItem value={"ACTIVE"}>ACTIVE</MenuItem>
+                <MenuItem value={"INACTIVE"}>INACTIVE</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="col-span-4 gap-8 md:col-span-2">
+            <TextField
+              name="tech_stack"
+              id="tech_stack"
+              label="Tech Stack"
+              inputProps={{ maxLength: 250 }}
+              size="small"
+              value={tech_stack}
+              onChange={handleAssetForm}
+              required
+            />
+          </div>
+        </section>
+
+        <section className="my-2 grid grid-cols-4 gap-8">
+          <div className="col-span-4 md:col-span-2">
+            <TextField
+              name="third_party_components"
+              id="third_party_components"
+              label="Third Party Components"
+              inputProps={{ maxLength: 250 }}
+              size="small"
+              value={third_party_components}
+              onChange={handleAssetForm}
+              required
+            />
+          </div>
+          <div className="col-span-4 gap-8 md:col-span-2">
+            <FormControl fullWidth>
+              <InputLabel id="assetType">Internal/External</InputLabel>
+              <Select
+                aria-label="search select"
+                label="Internal/External"
+                name="internal_external"
+                id="internal_external"
+                size="small"
+                value={internal_external}
+                onChange={handleAssetForm}
+                required
+              >
+                <MenuItem value={"Internal"}>Internal</MenuItem>
+                <MenuItem value={"External"}>External</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+        </section>
+
+        <section className="mt-4 w-full">
           <TextField
-            name="asset_name"
-            id="AssetName"
-            className="w-full py-2 px-2 focus:outline-none border-2 border-gray-400 h-11 resize-none"
-            label="Asset Name"
-            variant="outlined"
-            size="small"
-            inputProps={{ maxLength: 80 }}
-            value={asset_name}
+            name="additional_details"
+            id="additional_details"
+            multiline
+            inputProps={{ maxLength: 600 }}
+            rows={4}
+            sx={{ width: "100%" }}
+            label="Additional Details"
+            value={additional_details}
             onChange={handleAssetForm}
             required
           />
+        </section>
+        <div className="mt-3 flex items-center justify-between">
+          <button
+            type="submit"
+            className="rounded-md bg-gray-cus-5 px-14 py-2 tracking-wide text-white"
+          >
+            {isEdit ? "Save" : "Add"}
+          </button>
+          <button
+            type="button"
+            className="rounded-md bg-slate-200 px-4 py-2 text-gray-500"
+            onClick={handleClose}
+          >
+            Cancel
+          </button>
         </div>
-        <div className="md:col-span-2 col-span-4">
-          <FormControl fullWidth>
-            <InputLabel id="assetType">Asset Type</InputLabel>
-            <Select
-              aria-label="search select"
-              name="asset_type"
-              id="assetType"
-              size="small"
-              label="Asset Type"
-              value={asset_type}
-              onChange={handleAssetForm}
-              required
-            >
-              {AssetType.map((item) => {
-                return (
-                  <MenuItem value={item} key={item}>
-                    {item}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-        </div>
-      </section>
-
-      <section className="grid grid-cols-4 gap-8 my-4">
-        <div className="md:col-span-2 col-span-4">
-          <FormControl fullWidth>
-            <InputLabel id="Status">Status</InputLabel>
-            <Select
-              aria-label="search select"
-              labelId="Status"
-              name="status"
-              id="Status"
-              size="small"
-              label="Status"
-              value={status}
-              onChange={handleAssetForm}
-              required
-            >
-              <MenuItem value={"ACTIVE"}>ACTIVE</MenuItem>
-              <MenuItem value={"INACTIVE"}>INACTIVE</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div className="md:col-span-2 gap-8 col-span-4">
-          <TextField
-            name="tech_stack"
-            id="tech_stack"
-            label="Tech Stack"
-            inputProps={{ maxLength: 250 }}
-            size="small"
-            value={tech_stack}
-            onChange={handleAssetForm}
-            required
-          />
-        </div>
-      </section>
-
-      <section className="grid gap-8 grid-cols-4 my-2">
-        <div className="md:col-span-2 col-span-4">
-          <TextField
-            name="third_party_components"
-            id="third_party_components"
-            label="Third Party Components"
-            inputProps={{ maxLength: 250 }}
-            size="small"
-            value={third_party_components}
-            onChange={handleAssetForm}
-            required
-          />
-        </div>
-        <div className="md:col-span-2 gap-8 col-span-4">
-          <FormControl fullWidth>
-            <InputLabel id="assetType">Internal/External</InputLabel>
-            <Select
-              aria-label="search select"
-              label="Internal/External"
-              name="internal_external"
-              id="internal_external"
-              size="small"
-              value={internal_external}
-              onChange={handleAssetForm}
-              required
-            >
-              <MenuItem value={"Internal"}>Internal</MenuItem>
-              <MenuItem value={"External"}>External</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-      </section>
-
-      <section className="w-full mt-4">
-        <TextField
-          name="additional_details"
-          id="additional_details"
-          multiline
-          inputProps={{ maxLength: 600 }}
-          rows={4}
-          sx={{ width: "100%" }}
-          label="Additional Details"
-          value={additional_details}
-          onChange={handleAssetForm}
-          required
-        />
-      </section>
-      <div className="flex justify-between items-center mt-3">
-        <button
-          type="submit"
-          className="px-14 text-white py-2 rounded-md tracking-wide bg-gray-cus-5"
-        >
-          {isEdit ? "Save" : "Add"}
-        </button>
-        <button
-          type="button"
-          className="px-4 py-2 text-gray-500 bg-slate-200 rounded-md"
-          onClick={handleClose}
-        >
-          Cancel
-        </button>
       </div>
     </form>
   );
